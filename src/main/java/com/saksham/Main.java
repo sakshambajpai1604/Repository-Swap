665206d8629a7ec2261feec2890290947021838d
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2026-06-19 12:54:20 +0530

    feat: demonstrate repository swapping

--- src/main/java/com/saksham/Main.java
+++ src/main/java/com/saksham/Main.java
@@ -1,17 +1,31 @@
 package com.saksham;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        System.out.println("=== In Memory Repository ===");
+
+        UserRepository memoryRepo = new InMemoryUserRepository();
+        UserService memoryService = new UserService(memoryRepo);
+
+        memoryService.registerUser(1, "Alice");
+        memoryService.registerUser(2, "Bob");
+
+        memoryService.displayAllUsers();
+        memoryService.findUser(1);
+
+        System.out.println();
+
+        System.out.println("=== JPA Style Repository ===");
+
+        UserRepository jpaRepo = new JpaUserRepository();
+        UserService jpaService = new UserService(jpaRepo);
+
+        jpaService.registerUser(1, "Alice");
+        jpaService.registerUser(2, "Bob");
+
+        jpaService.displayAllUsers();
+        jpaService.findUser(1);
     }
 }
